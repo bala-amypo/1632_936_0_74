f20302c2a2812d1ac70342790c1a3f0c400bec08
bala-amypo <bala@amypo.in>
2025-12-18 08:09:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -15,9 +15,11 @@ public class ValidationEntity{
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     @NotNull
-    @Size(min=2,max=0,message )
+    @Size(min=2,max=10,message = "must be 2 to 10 character")
     private String username;
     private String email;
+    @Max(6)
+    @NotNull(message ="Password is mandatory")
     private String password;
     private int age;
 
