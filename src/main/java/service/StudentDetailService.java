b12b54acf512e7055a21da99ef467141cb464e6d
bala-amypo <bala@amypo.in>
2025-12-19 10:32:29 +0000

    User commit

---
+++ src/main/java/service/StudentDetailService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.StudentDetailEntity;
+
+
+
+public interface StudentDetailService{
+
+    //method declarations
+
+    StudentDetailEntity postData(StudentDetailEntity stu);   
+}
---
+++ src/main/java/service/StudentIdService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.StudentIdEntity;
+
+
+
+public interface StudentIdService{
+
+    //method declarations
+
+    StudentIdEntity postData(StudentIdEntity stu);   
+}
