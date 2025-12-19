3be0fe6cc21e79a1c7a0c43fa0b91c16bfa51770
bala-amypo <bala@amypo.in>
2025-12-19 06:03:54 +0000

    User commit

--- src/main/java/com/example/demo/execption/GlobalException.java
+++ src/main/java/com/example/demo/execption/GlobalException.java
@@ -2,7 +2,7 @@ package com.example.demo.exception;
 
 import.org.springframework.http.httpStatus;
 import.org.springframework.web.bind.annotation.ExceptionHandler;
-import.org.springframework.ResponseEntity;
+import.org.springframework.http.ResponseEntity;
 import.org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestcontrollerAdvice
